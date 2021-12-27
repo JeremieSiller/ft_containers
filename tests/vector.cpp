1f5107a7bdb15a354cb204e12e306929bcbffcaa
Jeremie Siller <jsiller@1-D-13.42heilbronn.de>
2021-12-27 16:12:44 +0100

    [vector] assign with size added

--- tests/vector.cpp
+++ tests/vector.cpp
@@ -16,13 +16,6 @@ using std::endl;
 template<class T>
 void	print_vector(vector<T> const &test)
 {
-	cout << endl;
-	cout << "-------------------------------" << endl;
-	cout << "get stuff from vector: " << endl;
-	cout << "capcity: " << test.capacity() << std::endl;
-	cout << "size: " << test.size() << std::endl;
-	cout << "max_size: " << test.max_size() << std::endl;
-	cout << "begin: " << *test.begin() << std::endl;
 	cout << "Printing all vector values: " << endl;
 	typename vector<T>::const_iterator begin = test.begin();
 	typename vector<T>::const_iterator end = test.end();
@@ -33,6 +26,13 @@ void	print_vector(vector<T> const &test)
 		if (begin != end)
 			cout << ", ";
 	}
+	cout << endl;
+	cout << "-------------------------------" << endl;
+	cout << "get stuff from vector: " << endl;
+	cout << "capcity: " << test.capacity() << std::endl;
+	cout << "size: " << test.size() << std::endl;
+	cout << "max_size: " << test.max_size() << std::endl;
+	cout << "begin: " << *test.begin() << std::endl;
 	// cout << "end: " << *test.end() << std::endl;
 	cout << "-------------------------------" << endl;
 	cout << "try out of range: (-1)" << endl;
@@ -75,7 +75,17 @@ int	main(void)
 		x.pop_back();
 		print_vector<int>(x);
 	}
-	x.pop_back();
-	// x.resize(-1);
+	print_vector<int>(x);
+	x.assign(5, 3);
+	try {
+		x.assign(INT_MAX, -10);
+	}
+	catch (std::exception const &e){
+		std::cerr << e.what() << std::endl;
+		std::cerr << e.what() << std::endl;
+		std::cerr << e.what() << std::endl;
+		std::cerr << e.what() << std::endl;
+		std::cerr << e.what() << std::endl;
+	}
 	print_vector<int>(x);
 }
