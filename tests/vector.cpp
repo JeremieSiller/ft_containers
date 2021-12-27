3a57d56d6d21d490fa4139a48167afff2271c050
Jeremie Siller <jsiller@1-D-13.42heilbronn.de>
2021-12-27 15:58:18 +0100

    [vector] implemnting pop and push

--- tests/vector.cpp
+++ tests/vector.cpp
@@ -16,6 +16,13 @@ using std::endl;
 template<class T>
 void	print_vector(vector<T> const &test)
 {
+	cout << endl;
+	cout << "-------------------------------" << endl;
+	cout << "get stuff from vector: " << endl;
+	cout << "capcity: " << test.capacity() << std::endl;
+	cout << "size: " << test.size() << std::endl;
+	cout << "max_size: " << test.max_size() << std::endl;
+	cout << "begin: " << *test.begin() << std::endl;
 	cout << "Printing all vector values: " << endl;
 	typename vector<T>::const_iterator begin = test.begin();
 	typename vector<T>::const_iterator end = test.end();
@@ -26,33 +33,26 @@ void	print_vector(vector<T> const &test)
 		if (begin != end)
 			cout << ", ";
 	}
-	cout << endl;
-	cout << "-------------------------------" << endl;
-	cout << "get stuff from vector: " << endl;
-	cout << "capcity: " << test.capacity() << std::endl;
-	cout << "size: " << test.size() << std::endl;
-	cout << "max_size: " << test.max_size() << std::endl;
-	cout << "begin: " << *test.begin() << std::endl;
-	cout << "end: " << *test.end() << std::endl;
+	// cout << "end: " << *test.end() << std::endl;
 	cout << "-------------------------------" << endl;
 	cout << "try out of range: (-1)" << endl;
 	try {
-		T	val = test[-1];
+		// T	val = test[-1];
 		cout << "doesnt throw error" << endl;
 	}
 	catch (std::exception const &e)
 	{
-		cout << "throws error: " << e.what() << endl;
+		cout << "throws error" << endl;
 	}
 	cout << "-------------------------------" << endl;
 	cout << "try out of range: (size)" << endl;
 	try {
-		T	val = test[test.size() + 1];
+		T	val = test.at(test.size());
 		cout << "doesnt throw error" << endl;
 	}
 	catch (std::exception const &e)
 	{
-		cout << "throws error: " << e.what() << endl;
+		cout << "throws error" << endl;
 	}
 	cout << "-------------------------------" << endl;
 	cout << "printing front value: " << endl;
@@ -63,6 +63,19 @@ void	print_vector(vector<T> const &test)
 int	main(void)
 {
 	vector<int>	x(10, 10);
+	print_vector<int>(x);
 	x.resize(22, -10);
+	print_vector<int>(x);
+	x.push_back(97);
+	print_vector<int>(x);
+	x.pop_back();
+	print_vector<int>(x);
+	while (x.size())
+	{
+		x.pop_back();
+		print_vector<int>(x);
+	}
+	x.pop_back();
+	// x.resize(-1);
 	print_vector<int>(x);
 }
