015b49ed763e36dfc7692a5ed56877e3aaa26bb6
Jeremie Siller <jsiller@1-D-13.42heilbronn.de>
2021-12-29 18:15:27 +0100

    relational test added

--- tests/vector.cpp
+++ tests/vector.cpp
@@ -36,35 +36,47 @@ void	print_vector(vector<T> const &test)
 	// cout << "end: " << *test.end() << std::endl;
 	cout << "-------------------------------" << endl;
 	cout << "try out of range: (-1)" << endl;
-	try {
-		// T	val = test[-1];
-		cout << "doesnt throw error" << endl;
-	}
-	catch (std::exception const &e)
-	{
-		cout << "throws error" << endl;
-	}
-	cout << "-------------------------------" << endl;
-	cout << "try out of range: (size)" << endl;
-	try {
-		T	val = test.at(test.size());
-		cout << "doesnt throw error" << endl;
-	}
-	catch (std::exception const &e)
-	{
-		cout << "throws error" << endl;
-	}
+	// try {
+	// 	// T	val = test[-1];
+	// 	cout << "doesnt throw error" << endl;
+	// }
+	// catch (std::exception const &e)
+	// {
+	// 	cout << "throws error" << endl;
+	// }
+	// cout << "-------------------------------" << endl;
+	// cout << "try out of range: (size)" << endl;
+	// try {
+	// 	T	val = test.at(test.size());
+	// 	cout << "doesnt throw error" << endl;
+	// }
+	// catch (std::exception const &e)
+	// {
+	// 	cout << "throws error" << endl;
+	// }
 	cout << "-------------------------------" << endl;
 	cout << "printing front value: " << endl;
 	cout << "front: " << test.front() << endl;
 	cout << "-------------------------------" << endl;
 }
 
+class testClass {
+public:
+	std::string str;
+	int	val;
+private:
+	testClass &operator=(testClass const &in) { return *this; };
+public:
+	testClass() : val() { }
+	testClass(int v) : val(v) { }
+	~testClass() { }
+};
+
 int	main(void)
 {
-	vector<int>	x(10, 10);
+	vector<int>	x(10, 9);
 	print_vector<int>(x);
-	x.resize(22, -10);
+	x.resize(22, 13);
 	print_vector<int>(x);
 	x.push_back(97);
 	print_vector<int>(x);
@@ -78,6 +90,66 @@ int	main(void)
 	print_vector<int>(x);
 	x.assign(5, 3);
 	print_vector<int>(x);
-	x.insert(++(x.end()), 10);
-	print_vector<int>(x);
+	x.insert((x.begin()), 1, 72);
+	print_vector(x);
+	x.insert(++(x.begin()), 1, 73);
+	print_vector(x);
+	x.insert((x.end()), 1, 432);
+	print_vector(x);
+	x.insert(--(x.end()), 1, 90);
+	print_vector(x);
+	std::cout << *x.insert(--x.end(), 33) << std::endl;
+	print_vector(x);
+	std::cout << *x.erase(--(x.end())) << std::endl;
+	print_vector(x);
+	std::cout << *x.erase(++x.begin(), --x.end()) << std::endl;
+	print_vector(x);
+	std::cout << *x.erase(++x.begin(), --x.end()) << std::endl;
+	print_vector(x);
+	vector<int>	t;
+	t = x;
+	print_vector(t);
+	t.push_back(-2353);
+	print_vector(t);
+	t.push_back(-2323);
+	print_vector(t);
+	t.push_back(12312);
+	print_vector(t);
+	x = t;
+	print_vector(x);
+	print_vector(t);
+	t.push_back(-123);
+	x[0] = 666;
+	std::cout << *x.begin() << std::endl;
+	std::cout << *t.begin() << std::endl;
+	x.swap(t);
+	std::cout << *x.begin() << std::endl;
+	std::cout << *t.begin() << std::endl;
+	print_vector(x);
+	print_vector(t);
+	std::cout << x.size() << std::endl;
+	std::cout << t.size() << std::endl;
+	std::cout << (t != x) << std::endl;
+	std::cout << (t == x) << std::endl;
+	std::cout << (t < x) << std::endl;
+	std::cout << (t > x) << std::endl;
+	std::cout << (t <= x) << std::endl;
+	std::cout << (t >= x) << std::endl;
+	t = x;
+	std::cout << (t != x) << std::endl;
+	std::cout << (t == x) << std::endl;
+	std::cout << (t < x) << std::endl;
+	std::cout << (t <= x) << std::endl;
+	std::cout << (t > x) << std::endl;
+	std::cout << (t >= x) << std::endl;
+	t.push_back(-123);
+	swap(x, t);
+	print_vector(x);
+	std::cout << (t != x) << std::endl;
+	std::cout << (t == x) << std::endl;
+	std::cout << (t < x) << std::endl;
+	std::cout << (t <= x) << std::endl;
+	std::cout << (t > x) << std::endl;
+	std::cout << (t >= x) << std::endl;
+	print_vector(t);
 }
