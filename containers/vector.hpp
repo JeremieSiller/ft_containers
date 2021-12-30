0515c224a2e16f36fb3ae9fe097abf724e30dae1
Jeremie Siller <jeremie@Jeremies-MacBook-Air.local>
2021-12-30 10:58:59 +0100

    fixes and map start

--- containers/vector.hpp
+++ containers/vector.hpp
@@ -97,10 +97,10 @@ namespace ft {
 		iterator				end()				{ return iterator(_end);					};
 		const_iterator			begin()		const	{ return const_iterator(_start);			};
 		const_iterator			end()		const	{ return const_iterator(_end);				};
-		reverse_iterator		rbegin()			{ return reverse_iterator(_start);			};
-		reverse_iterator		rend()				{ return reverse_iterator(_end);			};
-		const_reverse_iterator	rbegin()	const	{ return const_reverse_iterator(_start);	};
-		const_reverse_iterator	rend()		const	{ return const_reverse_iterator(_end);		};
+		reverse_iterator		rbegin()			{ return reverse_iterator(end());			};
+		reverse_iterator		rend()				{ return reverse_iterator(begin());			};
+		const_reverse_iterator	rbegin()	const	{ return const_reverse_iterator(end());	};
+		const_reverse_iterator	rend()		const	{ return const_reverse_iterator(begin());		};
 
 		/* --- size and capacity --- */
 		size_type	size ()													const	{ return _end - _start; }
