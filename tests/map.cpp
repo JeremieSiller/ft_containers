dcc4cb11db0fd6a0177721bee0ca3f8e9c321457
Jeremie Siller <jeremie@Jeremies-MacBook-Air.local>
2021-12-30 10:59:20 +0100

    test update vector

---
+++ tests/map.cpp
@@ -0,0 +1,20 @@
+#ifdef FT
+	#include "../containers/map.hpp"
+#else
+	#include <map>
+	namespace ft=std;
+#endif
+
+#include <iostream>
+#include <string>
+#include <limits.h>
+
+using ft::map;
+using std::cout;
+using std::endl;
+using std::string;
+
+int	main(void) {
+	map<string, int> test;
+
+}
