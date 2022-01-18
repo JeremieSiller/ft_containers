f2bed5152413aa0b3ef164b5f2aa76e95c6c6452
Jeremie Siller <jsiller@1-D-9.42heilbronn.de>
2022-01-18 18:24:55 +0100

    cleanup

--- containers/map.hpp
+++ containers/map.hpp
@@ -9,42 +9,12 @@
 #include "../utils/pair.hpp"
 #include "../utils/node_iterator.hpp"
 #include "../utils/iterator.hpp"
+#include <utility>
+#ifndef nullptr
+# define nullptr NULL
+#endif
 
 namespace ft {
-	// struct BaseNode {
-	// 	BaseNode	*parent;
-	// 	BaseNode	*left;
-	// 	BaseNode	*right;
-	// }
-	// template<
-	// 		class T,
-	// 		class Allcator = std::allocator<T>
-	// >
-	// struct Node : public BaseNode {
-	// public:
-	// 	typedef	T			value_type;
-	// 	typedef Allcator	allocator_type;
-	// 	typedef	typename allocator_type::reference			reference;
-	// 	typedef	typename allocator_type::const_reference	const_reference;
-	// 	typedef	typename allocator_type::pointer			pointer
-	// 	typedef	typename allocator_type::const_pointer		const_pointer
-	// public:
-	// 	value_type	value;
-	// private:
-	// 	allocator	_a;
-	// public:
-	// 	Node(const_reference val = value_type(), allocator_type alloc) {
-	// 		parent = nullptr;
-	// 		left = nullptr;
-	// 		right = nullptr;
-	// 		_a.construct(&value, val);
-	// 	}
-		 
-	// };
-
-	// struct NullNode : public BaseNode {
-
-	// }
 
 	template<
 		class Key,
@@ -88,6 +58,7 @@ namespace ft {
 				Node		*left;
 				Node		*right;
 				bool		is_end;
+				bool		colour;
 			public:
 				Node(bool _end) : value(), parent(nullptr), left(nullptr), right(nullptr), is_end(_end) { }
 				Node(const_reference val=value_type()) :
@@ -100,10 +71,9 @@ namespace ft {
 			typedef		ft::reverse_iterator<iterator>							reverse_iterator;
 			typedef		ft::reverse_iterator<const_iterator>					const_reverse_iterator;
 		private:
-			typedef		std::allocator<Node>								node_allocator;
+			typedef typename Allocator::template rebind<Node>::other	node_allocator;
 			Node					*_parent;
 			Node					*_last;
-			Node					*_first;
 			allocator_type			_a;
 			node_allocator			_na;
 			value_compare			_cmp;
@@ -141,19 +111,46 @@ namespace ft {
 				_na.deallocate(n, 1);
 			}
 
+			Node	*highestSubNode(Node *n) {
+				if (!n)
+					return nullptr;
+				while (n->right != nullptr && n->right->is_end == false) {
+					n = n->right;
+				}
+				return (n);
+			}
+
+			Node	*lowestSubNode(Node *n) {
+				if (!n)
+					return nullptr;
+				while (n->left != nullptr && n->left->is_end == false) {
+					n = n->left;
+				}
+				return (n);
+			}
+
+			size_type	_size(Node	*head) const {
+				if (head == nullptr || head->is_end == true) {
+					return 0;
+				} else {
+					return 1 + _size(head->left) + _size(head->right);
+				}
+			}
+
 		public:
 			explicit map (const key_compare& comp = key_compare(),
-				const allocator_type& alloc = allocator_type()) : _parent(nullptr), _last(nullptr), _first(nullptr), _a(alloc), _na(), _cmp(comp), _keycmp(comp) { }
+				const allocator_type& alloc = allocator_type()) : _parent(nullptr), _last(nullptr), _a(alloc), _na(), _cmp(comp), _keycmp(comp) { }
 			
+			~map() {
+				clear();
+			}
 			ft::pair<iterator, bool>	insert(const value_type &val) {
 				if (_parent == nullptr)
 				{
 					_parent = createNode(val);
-					_last = createEnd();
-					_first = createEnd();
+					if (!_last)
+						_last = createEnd();
 					_parent->right = _last;
-					_parent->left = _first;
-					_first->parent = _parent;
 					_last->parent = _parent;
 					return ft::pair<iterator, bool>(iterator(_parent), true);
 				}
@@ -218,16 +215,16 @@ namespace ft {
 						}
 					}
 
-			iterator	begin() { return iterator(_first->parent); }
-			const_iterator	begin() const { return const_iterator(_first->parent); }
+			iterator	begin() { return iterator(lowestSubNode(_parent)); }
+			const_iterator	begin() const { return const_iterator(lowestSubNode(_parent)); }
 			const_iterator	end()	const { return const_iterator(_last);}
 			iterator	end()	{ return iterator(_last);}
 			reverse_iterator rbegin() { return reverse_iterator(_last); }
 			const_reverse_iterator rbegin() const { return const_reverse_iterator(_last); }
-			reverse_iterator rend() { return reverse_iterator(_first->parent); }
-			const_reverse_iterator rend() const { return const_reverse_iterator(_first->parent); }
+			reverse_iterator rend() { return reverse_iterator(lowestSubNode(_parent)); }
+			const_reverse_iterator rend() const { return const_reverse_iterator(lowestSubNode(_parent)); }
 			bool		empty()	const { return(_parent == nullptr); }
-			size_type	size()	const { return(ft::distance(begin(), end()));}
+			// size_type	size()	const { return(ft::distance(begin(), end()));}
 			size_type	max_size() const { return _na.max_size(); }
 
 			iterator find (const key_type& k) {
@@ -289,56 +286,126 @@ namespace ft {
 
 			void erase (iterator position) {
 				Node	*n = position.base();
-
-				if (n != _parent) {
-					if (n->parent && n->parent->left == n) {
-						n->parent->left = n->left;
-						if (n->parent->left)
-							n->parent->left->right = n->right;
-						std::cout << "JO" << std::endl;
-						std::cout << n->value.first << std::endl;
+				if (n != _parent)
+				{
+					if (n->parent->left == n)
+					{
+						if (n->right)
+						{
+							Node *sub = lowestSubNode(n->right);
+							n->parent->left = n->right;
+							n->right->parent = n->parent;
+							sub->left = n->left;
+							if (sub->left)
+								sub->left->parent = sub;
+						}
+						else if (n->left)
+						{
+							n->left->parent = n->parent;
+							n->parent->left = n->left;
+						}
+						else {
+							n->parent->left = nullptr;
+						}
 					}
 					else {
-						n->parent->right = n->right;
-						n->parent->right->left = n->left;
+						if (n->left)
+						{
+							Node *sub = highestSubNode(n->left);
+							n->parent->right = n->left;
+							n->left->parent = n->parent;
+							sub->right = n->right;
+							if (sub->right)
+								sub->right->parent = sub;
+							if (n == _last->parent) {
+								_last->parent = highestSubNode(_parent);
+							}
+						}
+						else if (n->right)
+						{
+							n->right->parent = n->parent;
+							n->parent->right = n->right;
+						}
+						else {
+							n->parent->right = nullptr;
+						}
 					}
-					std::cout << "lol" << std::endl;
 				}
 				else {
-					Node	*next = --n;
-					n++;
-					if (next->parent && next->parent->left == next) {
-						next->parent->left = next->left;
-					} 
-					else {
-						next->parent->right = next->right;
+					if (n->left && n->left->is_end == false) {
+						Node	*sub;
+						_parent = n->left;
+						_parent->parent = nullptr;
+						sub = highestSubNode(_parent);
+						sub->right = n->right;
+						if (sub->right) {
+							sub->right->parent = sub;
+						}
+						_parent->right->parent =_parent;
 					}
-					next->right = n->right;
-					next->left = n->left;
-					next->parent = nullptr;
-					_parent = next;
+					else if (n->right && n->right->is_end == false) {
+						Node	*sub;
+						_parent = n->right;
+						_parent->parent = nullptr;
+						sub = lowestSubNode(_parent);
+						if (sub->left) {
+							sub->left->parent = sub;
+						}
+						sub->left = n->left;
+						_parent->left->parent = _parent;
+					}
+					else {
+						_parent = nullptr;
+					}
 				}
 				deleteNode(n);
 			}
 
-			size_type erase (const key_type& k);
+			size_type erase (const key_type& k) {
+				iterator it = find(k);
+				if (it == end())
+					return (0);
+				this->erase(it);
+				return (1);
+			}
 
-			void erase (iterator first, iterator last);
+			void erase (iterator first, iterator last) {
+				for (; first != last; ) { //could be way more efficient I guess 
+					this->erase(first++);
+				}
+			}
+
+			size_type	size() const {
+				return _size(_parent);
+			}
+
+			template<typename S>
+				void _swap(S &l, S& r) {
+					S	tmp = l;
+					l = r;
+					r = tmp;
+				}
+			void		swap(map &x) {
+				_swap(_parent, x._parent);
+				_swap(_last, x._last);
+				_swap(_a, x._a);
+				_swap(_na, x._na);
+				_swap(_cmp, x._cmp);
+				_swap(_keycmp, x._keycmp);
+			}
+		private:
+			void	_clear(Node *root) {
+				if (!root)
+					return ;
+				_clear(root->left);
+				_clear(root->right);
+				deleteNode(root);
+			}
+		public:
+			void		clear() {
+				_clear(_parent);
+			}
 	};
 }
-// {
-// 	// public:
-// 	// 	typedef		Key												key_type;
-// 	// 	typedef		T												mapped_type;
-// 	// 	typedef		ft:pair<const key_type, mapped_type>			value_type;
-// 	// 	typedef		Allocator										allocator_type;
-// 	// 	typedef		typename allocator_type::reference				reference;
-// 	// 	typedef		typename allocator_type::const_reference		const_reference;
-// 	// 	typedef		typename allocator_type::pointer				pointer;
-// 	// 	typedef		typename allocator_type::const_pointer			const_poiner;
-
-		
-// 	// };
-// }
 
 #endif
